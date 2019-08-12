1c21367178e30ad5b7db40f29b39c7c8011be4e0
zongzhec <zongzhe_chen@sina.com>
2019-08-12 20:54:48 +0800

    Add unit tests

--- src/test/java/foo/zongzhe/comon/utils/LogUtilTest.java
+++ src/test/java/foo/zongzhe/comon/utils/LogUtilTest.java
@@ -1,11 +1,16 @@
 package foo.zongzhe.comon.utils;
 
+import foo.zongzhe.common.utils.LogUtil;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
 import org.junit.runners.Suite;
 
-@RunWith(Suite.class)
 public class LogUtilTest {
+
+    @Test
+    public void printInfoTest(){
+        LogUtil.printInfo("printing info");
+    }
 
 }
