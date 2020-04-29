e624b5008f5a8205c8ca92039dbd10e1f03d35f1
zongzhec <zongzhe_chen@sina.com>
2020-04-29 19:08:22 +0800

    Add ArrayUtil

---
+++ src/main/java/foo/zongzhe/data_structure/array/ArrayUtil.java
@@ -0,0 +1,41 @@
+package foo.zongzhe.data_structure.array;
+
+
+public class ArrayUtil {
+
+    public static void printArray(int[][] inputArray) {
+        for (int i = 0; i < inputArray.length; i++) {
+            for (int j = 0; j < inputArray[i].length; j++) {
+                System.out.print(inputArray[i][j] + "\t");
+            }
+            System.out.println();
+        }
+    }
+
+    /**
+     * Initialize Array with default values
+     *
+     * @param array        Array to be initialized
+     * @param defaultValue default value
+     */
+    public static int[][] initArray(int[][] array, int defaultValue) {
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array[i].length; j++) {
+                array[i][j] = defaultValue;
+            }
+        }
+        return array;
+    }
+
+    /**
+     * Calculates "size" of an array, generally it is row * col
+     *
+     * @param array     Array to be calculate
+     * @param arrayDesc Array Description
+     */
+    public static void printArraySize(int[][] array, String arrayDesc) {
+        int row = array.length;
+        int col = array[0].length;
+        System.out.println(String.format("Size of %s is (%d * %d =) %d ", arrayDesc, row, col, (row * col)));
+    }
+}
--- src/main/java/foo/zongzhe/file/utils/FileUtil.java
+++ src/main/java/foo/zongzhe/file/utils/FileUtil.java
@@ -2,7 +2,9 @@ package foo.zongzhe.file.utils;
 
 import foo.zongzhe.common.utils.LogUtil;
 
-import java.io.File;
+import java.io.*;
+import java.util.ArrayList;
+import java.util.Scanner;
 
 /**
  * Utils used for common file process
@@ -17,5 +19,28 @@ public class FileUtil {
             fileExists = false;
         }
         return fileExists;
+    }
+
+    public static ArrayList<String> readFile(String path) {
+        ArrayList<String> fileContent = new ArrayList<>();
+        File file = new File(path);
+        InputStream is = null;
+        try {
+            is = new FileInputStream(file);
+        } catch (FileNotFoundException e) {
+            System.out.println("ERROR: File not found in " + path);
+            e.printStackTrace();
+        }
+        InputStreamReader reader = new InputStreamReader(is);
+        Scanner s = new Scanner(reader); //.useDelimiter("\t");
+        while (s.hasNextLine()) {
+            fileContent.add(s.nextLine());
+        }
+        try {
+            reader.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return fileContent;
     }
 }
